f75c03cb9871df02158c625d06d33a17947191b1
skoanton <antonhakanssonn@gmail.com>
2024-08-25 12:44:15 +0200

    kopplat min databas till backend, sskapat get och put api atm

---
+++ backend/main.go
@@ -0,0 +1,130 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host   = "localhost"
+	port   = 5432
+	user   = "postgres"
+	dbname = "todoapp"
+)
+
+var db *sql.DB
+
+type Todo struct {
+	ID        string `json:"id"`
+	Title     string `json:"title"`
+	DueDate   string `json:"due_date"`
+	Done      string `json:"done"`
+	CreatedAt string `JSON:"created_at`
+}
+
+func main() {
+
+	//Connection to db
+	psqlInfo := fmt.Sprintf("host=%s port=%d user =%s "+" dbname=%s sslmode=disable", host, port, user, dbname)
+	var err error
+	db, err = sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Connection successful")
+
+	http.HandleFunc("/todos", handleTodos)
+
+	fmt.Println("Server is running on port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
+}
+
+func handleTodos(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		getTodos(w, r)
+	case http.MethodPost:
+		createTodo(w, r)
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+}
+
+func createTodo(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var todo Todo
+	err := json.NewDecoder(r.Body).Decode(&todo)
+
+	if err != nil {
+		http.Error(w, "Bad request", http.StatusBadRequest)
+		return
+	}
+
+	var id int
+
+	err = db.QueryRow(`
+	INSERT INTO todos(title,due_date)
+	VALUES($1,$2)
+	RETURNING ID
+	`, todo.Title, todo.DueDate).Scan(&id)
+	if err != nil {
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]int{"id": id})
+}
+
+func getTodos(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	rows, err := db.Query("SELECT * FROM todos")
+
+	if err != nil {
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	defer rows.Close()
+
+	todos := []Todo{}
+
+	for rows.Next() {
+		var todo Todo
+		err := rows.Scan(&todo.ID, &todo.Title, &todo.DueDate, &todo.Done, &todo.CreatedAt)
+		if err != nil {
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return
+		}
+		todos = append(todos, todo)
+
+		if err := rows.Err(); err != nil {
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(todos)
+	}
+}
