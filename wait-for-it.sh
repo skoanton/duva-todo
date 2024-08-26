dfd53496aed5defe4c83b28c84a915f27ff2d105
skoanton <antonhakanssonn@gmail.com>
2024-08-26 14:23:34 +0200

    la till sh fil

---
+++ wait-for-it.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# wait-for-it.sh
+
+host="$1"
+shift
+port="$1"
+shift
+cmd="$@"
+
+until nc -z "$host" "$port"; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres is up - executing command"
+exec $cmd
