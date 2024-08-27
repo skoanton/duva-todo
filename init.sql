aa96ed36e37f186191b7c31ffbcf1b4cc0ccd63c
skoanton <antonhakanssonn@gmail.com>
2024-08-27 16:27:35 +0200

    satte pw i init

--- init.sql
+++ init.sql
@@ -1,3 +1,9 @@
+CREATE DATABASE todoapp;
+
+\c todoapp 
+
+ALTER USER postgres WITH PASSWORD 'password';
+
 CREATE TABLE todos (
     id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
@@ -7,20 +13,25 @@ CREATE TABLE todos (
 );
 
 INSERT INTO
-    todos (title, done, due_date)
+    todos (
+        title,
+        done,
+        due_date,
+        created_at
+    )
 VALUES (
         'Skriva en dokumentation',
         FALSE,
-        '2024-09-01'
+        '2024-09-01' '2024-09-01'
     ),
     (
         'Förbereda presentation',
         FALSE,
-        '2024-09-10'
+        '2024-09-10' '2024-09-10'
     ),
     ('Handla', TRUE, '2024-09-05'),
     (
         'Gå ut med hunden',
         FALSE,
-        '2024-09-15'
+        '2024-09-15' '2024-09-15'
     );
